61266287348f810d1f37c94754b7e8089aa72564
Josh Haas <jphaas@gmail.com>
2011-03-21 05:14:12 -0400

    adding server_interface

--- local.py
+++ local.py
@@ -5,7 +5,8 @@ import subprocess
 from shutil import rmtree
 import sys
 import shutil
-from jwl_make_lib import JWLReader, gen, need_regen, merge_source_file, project_to_path, clean_path
+from jwl_make_lib import JWLReader, gen, need_regen, merge_source_file, project_to_path, clean_path, resolve_import
+import tornado.web
 #import fabric.api as fab
 
 def sys_call(args,cwd=None):
@@ -16,26 +17,33 @@ def sys_call(args,cwd=None):
 def do_action(project, actionargs, deploypath, global_config):
     dependspath = join(deploypath, 'depends')
     codepath = join(deploypath, 'code')
+    staticpath = join(deploypath, 'static')
+    htmlpath = join(deploypath, 'html')
     
     reader = JWLReader(project)
     
-    if exists(codepath):
-        rmtree(codepath)
-    makedirs(codepath)
-    
-    if not exists(dependspath):
-        makedirs(dependspath)
+    for p in (dependspath, codepath, staticpath, htmlpath):
+        if exists(p):
+            rmtree(p)
+        makedirs(p)
         
-   
+    urlhandlers = []
     #create the html pages
     for sourcefile in reader.get_html():
-        gen(join(codepath, basename(sourcefile.path)), merge_source_file(sourcefile))
+        stripped_name = basename(sourcefile.path).rsplit('.', 1)[0]
+        gen(join(htmlpath, stripped_name), merge_source_file(sourcefile))
+        urlhandlers.append((r"/(%s)"%stripped_name, tornado.web.StaticFileHandler, {"path": htmlpath}))
         #pagenames.append(basename(sourcefile.path).rsplit('.', 1)[0])
  
+    urlhandlers.append((r"/()", tornado.web.StaticFileHandler, {"path": htmlpath, "default_filename": "index"}))
  
     #copy over any raw python files
     for file in reader.list_python():
         shutil.copy(file, join(codepath, basename(file)))
+        
+    
+    #get the javascript necessary for server_interface
+    server_interface_path = resolve_import('jwl_make2/server_interface.js', None)
     
     print 'fetching dependencies'
     
@@ -58,7 +66,7 @@ def do_action(project, actionargs, deploypath, global_config):
                     rmtree(dpath)
                     raise
             sys_call('git pull ' + url + ' master', dpath)
-    
+               
     #revert to the old path, then add dependencies
     del sys.path[:]
     sys.path.extend(clean_path)
@@ -69,8 +77,27 @@ def do_action(project, actionargs, deploypath, global_config):
     from jwl.DB.hashdb import HashDB
     deployconfig.set(dbengine=HashDB)
     deployconfig.set(debug=True)
+       
     
     import index
     from jwl.tornado_launch import launch
+    from jwl.remote_method import make_handler, make_dummy_handler
+    
+    #write javascript function handlers
+    from jwl import remote_method
+    
+    with open(join(htmlpath, 'server_interface.js'), 'w') as f:
+        f.write('var server = "%s";'%reader.server_prefix)
+        with open(server_interface_path, 'r') as f2:
+            f.write(f2.read())
+        f.write('\n')
+        f.write(make_dummy_handler(index.main).write_js_interface())
+        
+    urlhandlers.append((r"/(server_interface.js)", tornado.web.StaticFileHandler, {"path": htmlpath}))
+    
+    
     print 'starting local server...'
-    launch(index.main, reader.server_prefix, int(global_config.get('local', 'port')))
+    urlhandlers.append((r"/" + reader.server_prefix, make_handler(index.main)))
+    application = tornado.web.Application(urlhandlers)
+ 
+    launch(application, int(global_config.get('local', 'port')))
