02e779fc468de36573f901d4e0d8356660b25e60
Josh Haas <jphaas@gmail.com>
2011-06-15 16:26:24 -0400

    better errorchecking for bad callbacks part 2

--- server_interface.js
+++ server_interface.js
@@ -575,6 +575,9 @@ function displayMessage(message, obj)
 //wrapper on method_call_raw that displays an error
 function method_call(method, params, callback)
 {
+    if (typeof(callback) != 'function') {
+        throw 'callback must be a function, got: ' + callback;
+    }
     method_call_raw(method, params, function(success, data)
     {
         if (!success)
