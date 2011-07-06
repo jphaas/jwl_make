192734a2b43ed4cb0cd4692d5b6c4ab26df826a8
Josh Haas <jphaas@gmail.com>
2011-07-06 16:46:31 -0400

    adding autoswitch back to master

--- remote.py
+++ remote.py
@@ -47,191 +47,197 @@ def do_action(project, actionargs, deploypath, global_config):
     reader = JWLReader(project)
     
     #SWITCH TO RELEASE BRANCH
-    sys_call('git checkout release', reader.path)
-    
-    reader.compile_coffee()
-    
-
-    
-       
-    #SETUP DEPLOY CONFIG
-    envkey = target + '_'
-    dplines = ['from jwl import deployconfig']
-    config_data = {}
-    for section in reader._config.sections():
-        if section.startswith(envkey):
-            for key, value in reader._config.items(section):
-                sectiontitle = section[len(envkey):]
-                rvalue = repr(value)
-                dplines.append("deployconfig.set2('env.%(sectiontitle)s.%(key)s', %(rvalue)s)"%locals())
-                config_data['env.' + section[len(envkey):] + '.' + key] = value
-    config_data['env'] = target
-                
-    #server_side paths
-    server_deploypath = config_data['env.basic.deploypath']
-    server_dependspath = server_deploypath + '/depends'
-    server_codepath = server_deploypath + '/code'
-    server_staticpath = server_deploypath + '/static'
-    server_htmlpath = server_deploypath + '/html'
-    rserver_dependspath = repr(server_dependspath)
-    rserver_codepath = repr(server_codepath)
-    rserver_staticpath = repr(server_staticpath)
-    rserver_htmlpath = repr(server_htmlpath)
-    
-        
-    deployrepo = config_data['env.basic.deployrepo']
-    if not exists(deploypath):
-        makedirs(deploypath)
-        sys_call('git init', deploypath)
-        sys_call('git remote add origin ' + deployrepo, deploypath)
-        sys_call('git pull origin uploaded', deploypath)
-        sys_call('git branch uploaded', deploypath)
-        sys_call('git checkout uploaded', deploypath)
-    else: #clean but don't remove the .git directory
-        sys_call('git pull origin uploaded', deploypath)
-        for p in (dependspath, codepath, htmlpath, staticpath):
-            if exists(p): rmtree(p, onerror=onerror)
-        
-    for p in (dependspath, codepath, htmlpath, staticpath):
-        if not exists(p): makedirs(p)
-
-    
-    gen(join(codepath, 'deployconfig_init.py'), '\n'.join(dplines))
-    
-    #legacy...
-    config_data['facebook_app_id'] = config_data['env.facebook.facebook_app_id']
-        
-    urlhandlers = []
-    #create the html pages
-    for sourcefile in reader.get_html(config_data):
-        stripped_name = basename(sourcefile.path).rsplit('.', 1)[0]
-        gen(join(htmlpath, stripped_name), merge_source_file(sourcefile))
-        urlhandlers.append('urlhandlers.append((r"/(%(stripped_name)s)", tornado.web.StaticFileHandler, {"path": %(rserver_htmlpath)s}))'%locals())
- 
-    urlhandlers.append('urlhandlers.append((r"/()", tornado.web.StaticFileHandler, {"path": %(rserver_htmlpath)s, "default_filename": "index"}))'%locals())
-    
-    #copy over resources
-    if exists(staticpath):
-        rmtree(staticpath)
-    shutil.copytree(reader.resources, staticpath)
-    rprefix = reader.resource_prefix
-    urlhandlers.append('urlhandlers.append((r"/%(rprefix)s/(.*)", tornado.web.StaticFileHandler, {"path": %(rserver_staticpath)s}))'%locals())
- 
-    #copy over any raw python files
-    for file in reader.list_python():
-        shutil.copy(file, join(codepath, basename(file)))
-        
-      
-    print 'fetching dependencies'
-    
-    #fetch the dependencies
-    depends = reader.config_items('depends')
-    for name, url in depends:
-        dpath = join(dependspath, name)
-        if url.startswith('local:'):
-            url = url[6:]
-            if exists(dpath):
-                rmtree(dpath)
-            shutil.copytree(url, dpath, ignore=shutil.ignore_patterns('*.git', '*.svn'))
-        else:
-            if not exists(dpath):
-                try:
-                    makedirs(dpath)
-                    sys_call('git init', dpath)
-                    #sys_call('git remote add origin ' + url, dpath)
-                except:
-                    rmtree(dpath)
-                    raise
-            sys_call('git pull ' + url + ' master', dpath)
-    
-    cookie_secret = repr(reader.config('basic', 'cookie_secret'))
-    
-    # deal with in a non-legacy way at some point?
-    
-    # google_consumer_key = reader.config('google', 'consumer_key')
-    # google_consumer_secret = reader.config('google', 'consumer_secret')
-    
-    
-    #build server_interface.js
-    server_interface_path = resolve_import('jwl_make2/server_interface.js', None)
-    del sys.path[:]
-    sys.path.extend(clean_path)
-    sys.path.append(dependspath)
-    sys.path.append(codepath)
-    import index
-    from jwl.remote_method import make_dummy_handler
-    
-    with open(join(htmlpath, 'server_interface.js'), 'w') as f:
-        f.write('var server = "%s";'%reader.server_prefix)
-        with open(server_interface_path, 'r') as f2:
-            f.write(f2.read())
-        f.write('\n')
-        f.write(make_dummy_handler(index.main).write_js_interface())
-        
-    urlhandlers.append('urlhandlers.append((r"/(server_interface.js)", tornado.web.StaticFileHandler, {"path": %(rserver_htmlpath)s}))'%locals())
-    
-    urlhandlercode = '\n'.join(urlhandlers)
-    
-    readerserverprefix = reader.server_prefix
-    
-    is_debug = config_data['env.basic.debug']
-            
-    #build the execution file
-    launch_server = r"""
-import sys
-sys.path.append(%(rserver_dependspath)s)
-sys.path.append(%(rserver_codepath)s)
-
-import deployconfig_init
-from jwl import deployconfig
-
-deployconfig.set(debug=%(is_debug)s)
-   
-import index
-import tornado
-from jwl.tornado_launch import launch
-from jwl.remote_method import make_dummy_handler
-
-urlhandlers = []
-
-%(urlhandlercode)s 
-
-#GOOGLE LOGIN
-# from jwl.googleauth import LoginController
-# urlhandlers.append((r"/auth/(.*)", LoginController))
-
-if __name__ == '__main__':
-    print 'starting server...'
-    urlhandlers.append((r"/%(readerserverprefix)s", index.main))
-    application = tornado.web.Application(urlhandlers, cookie_secret=%(cookie_secret)s)#, google_consumer_key=google_consumer_key, google_consumer_secret=google_consumer_secret)
-
-launch(application, 80)
-    """%locals()
-    
-    gen(join(codepath, 'launch_server.py'), launch_server)
-    
-    print 'about to upload...'
-    
-    #check in the local code to git
-    sys_call('git add *', deploypath)
-    sys_call('git commit -m "automated..."', deploypath)
-    sys_call('git push origin uploaded', deploypath)
-    
-    keyfile = global_config.get('keys', config_data['env.basic.sshkey']) 
-    #Upload to server
-    import fabric.api as fab
     try:
-        with fab.settings(host_string=config_data['env.basic.host'],key_filename=keyfile,disable_known_hosts=True):
-            # with fab.settings(warn_only=True):
-                # remote_exists = not fab.run("test -d %s" % server_deploypath).failed
-            with fab.cd(server_deploypath):
-                with fab.settings(warn_only=True):
-                    fab.run('git add *')
-                    fab.run('git commit -m "saving any changes such as .pyc etc"')
-                fab.run('git merge uploaded')
-            fab.run(config_data['env.basic.startcommand'])
+        sys_call('git checkout release', reader.path)
+        
+        reader.compile_coffee()
+        
+
+        
+           
+        #SETUP DEPLOY CONFIG
+        envkey = target + '_'
+        dplines = ['from jwl import deployconfig']
+        config_data = {}
+        for section in reader._config.sections():
+            if section.startswith(envkey):
+                for key, value in reader._config.items(section):
+                    sectiontitle = section[len(envkey):]
+                    rvalue = repr(value)
+                    dplines.append("deployconfig.set2('env.%(sectiontitle)s.%(key)s', %(rvalue)s)"%locals())
+                    config_data['env.' + section[len(envkey):] + '.' + key] = value
+        config_data['env'] = target
+                    
+        #server_side paths
+        server_deploypath = config_data['env.basic.deploypath']
+        server_dependspath = server_deploypath + '/depends'
+        server_codepath = server_deploypath + '/code'
+        server_staticpath = server_deploypath + '/static'
+        server_htmlpath = server_deploypath + '/html'
+        rserver_dependspath = repr(server_dependspath)
+        rserver_codepath = repr(server_codepath)
+        rserver_staticpath = repr(server_staticpath)
+        rserver_htmlpath = repr(server_htmlpath)
+        
+            
+        deployrepo = config_data['env.basic.deployrepo']
+        if not exists(deploypath):
+            makedirs(deploypath)
+            sys_call('git init', deploypath)
+            sys_call('git remote add origin ' + deployrepo, deploypath)
+            sys_call('git pull origin uploaded', deploypath)
+            sys_call('git branch uploaded', deploypath)
+            sys_call('git checkout uploaded', deploypath)
+        else: #clean but don't remove the .git directory
+            sys_call('git pull origin uploaded', deploypath)
+            for p in (dependspath, codepath, htmlpath, staticpath):
+                if exists(p): rmtree(p, onerror=onerror)
+            
+        for p in (dependspath, codepath, htmlpath, staticpath):
+            if not exists(p): makedirs(p)
+
+        
+        gen(join(codepath, 'deployconfig_init.py'), '\n'.join(dplines))
+        
+        #legacy...
+        config_data['facebook_app_id'] = config_data['env.facebook.facebook_app_id']
+            
+        urlhandlers = []
+        #create the html pages
+        for sourcefile in reader.get_html(config_data):
+            stripped_name = basename(sourcefile.path).rsplit('.', 1)[0]
+            gen(join(htmlpath, stripped_name), merge_source_file(sourcefile))
+            urlhandlers.append('urlhandlers.append((r"/(%(stripped_name)s)", tornado.web.StaticFileHandler, {"path": %(rserver_htmlpath)s}))'%locals())
+     
+        urlhandlers.append('urlhandlers.append((r"/()", tornado.web.StaticFileHandler, {"path": %(rserver_htmlpath)s, "default_filename": "index"}))'%locals())
+        
+        #copy over resources
+        if exists(staticpath):
+            rmtree(staticpath)
+        shutil.copytree(reader.resources, staticpath)
+        rprefix = reader.resource_prefix
+        urlhandlers.append('urlhandlers.append((r"/%(rprefix)s/(.*)", tornado.web.StaticFileHandler, {"path": %(rserver_staticpath)s}))'%locals())
+     
+        #copy over any raw python files
+        for file in reader.list_python():
+            shutil.copy(file, join(codepath, basename(file)))
+            
+          
+        print 'fetching dependencies'
+        
+        #fetch the dependencies
+        depends = reader.config_items('depends')
+        for name, url in depends:
+            dpath = join(dependspath, name)
+            if url.startswith('local:'):
+                url = url[6:]
+                if exists(dpath):
+                    rmtree(dpath)
+                shutil.copytree(url, dpath, ignore=shutil.ignore_patterns('*.git', '*.svn'))
+            else:
+                if not exists(dpath):
+                    try:
+                        makedirs(dpath)
+                        sys_call('git init', dpath)
+                        #sys_call('git remote add origin ' + url, dpath)
+                    except:
+                        rmtree(dpath)
+                        raise
+                sys_call('git pull ' + url + ' master', dpath)
+        
+        cookie_secret = repr(reader.config('basic', 'cookie_secret'))
+        
+        # deal with in a non-legacy way at some point?
+        
+        # google_consumer_key = reader.config('google', 'consumer_key')
+        # google_consumer_secret = reader.config('google', 'consumer_secret')
+        
+        
+        #build server_interface.js
+        server_interface_path = resolve_import('jwl_make2/server_interface.js', None)
+        del sys.path[:]
+        sys.path.extend(clean_path)
+        sys.path.append(dependspath)
+        sys.path.append(codepath)
+        import index
+        from jwl.remote_method import make_dummy_handler
+        
+        with open(join(htmlpath, 'server_interface.js'), 'w') as f:
+            f.write('var server = "%s";'%reader.server_prefix)
+            with open(server_interface_path, 'r') as f2:
+                f.write(f2.read())
+            f.write('\n')
+            f.write(make_dummy_handler(index.main).write_js_interface())
+            
+        urlhandlers.append('urlhandlers.append((r"/(server_interface.js)", tornado.web.StaticFileHandler, {"path": %(rserver_htmlpath)s}))'%locals())
+        
+        urlhandlercode = '\n'.join(urlhandlers)
+        
+        readerserverprefix = reader.server_prefix
+        
+        is_debug = config_data['env.basic.debug']
+                
+        #build the execution file
+        launch_server = r"""
+    import sys
+    sys.path.append(%(rserver_dependspath)s)
+    sys.path.append(%(rserver_codepath)s)
+
+    import deployconfig_init
+    from jwl import deployconfig
+
+    deployconfig.set(debug=%(is_debug)s)
+       
+    import index
+    import tornado
+    from jwl.tornado_launch import launch
+    from jwl.remote_method import make_dummy_handler
+
+    urlhandlers = []
+
+    %(urlhandlercode)s 
+
+    #GOOGLE LOGIN
+    # from jwl.googleauth import LoginController
+    # urlhandlers.append((r"/auth/(.*)", LoginController))
+
+    if __name__ == '__main__':
+        print 'starting server...'
+        urlhandlers.append((r"/%(readerserverprefix)s", index.main))
+        application = tornado.web.Application(urlhandlers, cookie_secret=%(cookie_secret)s)#, google_consumer_key=google_consumer_key, google_consumer_secret=google_consumer_secret)
+
+    launch(application, 80)
+        """%locals()
+        
+        gen(join(codepath, 'launch_server.py'), launch_server)
+        
+        print 'about to upload...'
+        
+        #check in the local code to git
+        sys_call('git add *', deploypath)
+        sys_call('git commit -m "automated..."', deploypath)
+        sys_call('git push origin uploaded', deploypath)
+        
+        keyfile = global_config.get('keys', config_data['env.basic.sshkey']) 
+        #Upload to server
+        import fabric.api as fab
+        try:
+            with fab.settings(host_string=config_data['env.basic.host'],key_filename=keyfile,disable_known_hosts=True):
+                # with fab.settings(warn_only=True):
+                    # remote_exists = not fab.run("test -d %s" % server_deploypath).failed
+                with fab.cd(server_deploypath):
+                    with fab.settings(warn_only=True):
+                        fab.run('git add *')
+                        fab.run('git commit -m "saving any changes such as .pyc etc"')
+                    fab.run('git merge uploaded')
+                fab.run(config_data['env.basic.startcommand'])
+        finally:
+            from fabric.state import connections
+            for key in connections.keys():
+                connections[key].close()
+                del connections[key]
+                
+         
     finally:
-        from fabric.state import connections
-        for key in connections.keys():
-            connections[key].close()
-            del connections[key]
+        #SWITCH BACK TO MASTER BRANCH
+        sys_call('git checkout master', reader.path)
