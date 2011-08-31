f0d9ad1cedf7d6d6b8711341f8b766b269717ac3
Joshua Haas <jphaas@gmail.com>
2011-08-31 12:07:40 -0400

    fixing JSON

--- server_interface.js
+++ server_interface.js
@@ -1,6 +1,6 @@
 /*
     http://www.JSON.org/json2.js
-    2011-01-18
+    2011-02-23
 
     Public Domain.
 
@@ -208,7 +208,7 @@ if (!JSON) {
         rep;
 
 
-   /* function quote(string) {
+    function quote(string) {
 
 // If the string contains no control characters, no quote characters, and no
 // backslash characters, then we can safely slap some quotes around it.
@@ -221,18 +221,6 @@ if (!JSON) {
             return typeof c === 'string' ? c :
                 '\\u' + ('0000' + a.charCodeAt(0).toString(16)).slice(-4);
         }) + '"' : '"' + string + '"';
-    } */
-    
-    //josh's revised version because the original isn't working right and I don't understand it well enough to fix it in situ
-    function quote(string)
-    {
-        var ret = '';
-        for (var i = 0; i < string.length; i++)
-        {
-            c = string.charAt(i);
-            ret += meta[c] ? meta[c] : c;
-        }
-        return ret;
     }
 
 
@@ -327,8 +315,8 @@ if (!JSON) {
             if (rep && typeof rep === 'object') {
                 length = rep.length;
                 for (i = 0; i < length; i += 1) {
-                    k = rep[i];
-                    if (typeof k === 'string') {
+                    if (typeof rep[i] === 'string') {
+                        k = rep[i];
                         v = str(k, value);
                         if (v) {
                             partial.push(quote(k) + (gap ? ': ' : ':') + v);
@@ -340,7 +328,7 @@ if (!JSON) {
 // Otherwise, iterate through all of the keys in the object.
 
                 for (k in value) {
-                    if (Object.hasOwnProperty.call(value, k)) {
+                    if (Object.prototype.hasOwnProperty.call(value, k)) {
                         v = str(k, value);
                         if (v) {
                             partial.push(quote(k) + (gap ? ': ' : ':') + v);
@@ -425,7 +413,7 @@ if (!JSON) {
                 var k, v, value = holder[key];
                 if (value && typeof value === 'object') {
                     for (k in value) {
-                        if (Object.hasOwnProperty.call(value, k)) {
+                        if (Object.prototype.hasOwnProperty.call(value, k)) {
                             v = walk(value, k);
                             if (v !== undefined) {
                                 value[k] = v;
